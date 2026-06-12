0758805983ec953cef31a59c0b5972b07497aa7f
HariCoder200513 <harivigneshwaran2005@gmail.com>
2026-06-12 19:06:12 +0530

    fix:parse and table render

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 async function loadStocks() {
     const response = await fetch("http://127.0.0.1:5000/stocks");
     const data = await response.json();
-    console.log(data)
+
+    console.log(data);
 
     const table = document.getElementById("stockTable");
     table.innerHTML = "";
 
-    data.forEach(stock => {
+    const stocks = data.stocks;
+
+    stocks.forEach(stock => {
         table.innerHTML += `
             <tr>
                 <td>${stock.ticker}</td>
                 <td>${stock.name ?? "N/A"}</td>
                 <td>${stock.sector ?? "N/A"}</td>
-                <td>${stock.market_cap ?? "N/A"}</td>
+                <td>${stock.market_cap?.toLocaleString() ?? "N/A"}</td>
             </tr>
         `;
     });
